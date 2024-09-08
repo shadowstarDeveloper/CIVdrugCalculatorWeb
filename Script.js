deb9a064acfbe6cd291e3f2b0aa2b200e12dfddd
K0KK0R0 <shadowstarDeveloper@gmail.com>
2024-09-09 03:34:01 +0900

    20240908

--- Script.js
+++ Script.js
@@ -7,6 +7,7 @@ const BWTinput = document.querySelector('#bwtinput');
 const drugNameTxt = document.getElementsByClassName("drugnametxt");
 const drugDose = document.getElementsByClassName('drugdose');
 const fluidCC = document.getElementsByClassName('fluidcc');
+const doseBt = document.getElementsByClassName('dosebt');
 
 const drugSpeed = document.getElementsByClassName('drugspeed');
 const drugSpeedTxt = document.getElementsByClassName('drugspeedtxt');
@@ -29,13 +30,13 @@ const nextBt = document.getElementById("nextBt");
 
 //메인메뉴 데이터
 const mainMenuBt = document.getElementById("civIcon");
-const mainPage = document.getElementById("menuPanel");
+const menuPanel = document.getElementById("menuPanel");
 const menuX = document.getElementById("menuiconX");
 
 //메인메뉴 모달
 mainMenuBt.addEventListener("mousedown", function (e) {
     //메뉴 토글
-    mainPage.style.display = "inline";
+    menuPanel.style.display = "inline";
 })
 menuX.addEventListener("mousedown", fmodalClose);
 
@@ -51,7 +52,6 @@ function fmodalClose(e) {
     //프리셋 모달 끄기
     e.currentTarget.parentNode.style.display = "none";
 }
-
 addBt.addEventListener('click', function (e) {
     //추가 버튼
    addItem(null);
@@ -59,13 +59,28 @@ addBt.addEventListener('click', function (e) {
 window.addEventListener('input', function (e) {
     update();
 });
+window.addEventListener('keydown', function (e) {
 
+    
+    if (e.key == "Escape") {
+        //console.log(e.key);
+        if (presetPanel.style.display != "none") {
+            presetPanel.style.display = "none";
+        }
+        else if (menuPanel.style.display != "none") {
+            menuPanel.style.display = "none";
+        } else {
+            menuPanel.style.display = "inline";
+        }
+    }
+})
 //아이템 추가
 function addItem( object ){
      const item = drugItemID.content.cloneNode(true);
      const drugnameTxt = item.querySelector(".drugnametxt");
      const drugdoseTxt = item.querySelector(".drugdose");
-     const fluidccTxt = item.querySelector(".fluidcc");
+    const fluidccTxt = item.querySelector(".fluidcc");
+    const dosebt = item.querySelector(".dosebt");
      
      const drugspeedTxt = item.querySelector(".drugspeed");
      const drugspeedTxtbox = item.querySelector(".drugspeedtxt");
@@ -74,7 +89,11 @@ function addItem( object ){
      if(object !=null){
          drugnameTxt.value = object.drugName;
          drugdoseTxt.value = object.drugDose;
-         if(object.drugGram =="mcg"){ drugdoseTxt.value = object.drugDose*0.001}
+         dosebt.value = 1;
+         if (object.drugGram == "mcg") {
+             drugdoseTxt.value = object.drugDose;// * 0.001;
+             dosebt.value = 1000;
+         }
          fluidccTxt.value = object.fluidTotalcc;
          drugspeedTxt.value = object.drugSpeed;
          drugspeedTxtbox.value = object.drugSpeedtxt;
@@ -189,6 +208,8 @@ const update = function (value) {
     //
     for (let i = 0; i < drugResult.length; i++) {
         let _drugnametxt = drugNameTxt[i].value;
+        let _doseBt = doseBt[i].value;
+        //console.log(_doseBt);
         
         let _bwt = Number(BWTinput.value);
 
@@ -252,7 +273,7 @@ const update = function (value) {
         // 0.1mcg/kg/min*hr = cc
         //0.0001  * drugdose / bwt / 60 //mcg는 *1000
         //(data.drugSpeed * data.fluidTotalcc * Bwt * 60) / (data.drugDose * dose);
-        let _result = (_drugspeed * _fluidcc * _bwt * _min) / (_drugdose * _dose);
+        let _result = (_drugspeed * _fluidcc * _bwt * _min) / (_drugdose * _dose) * 1000 / _doseBt;
 
         drugResult[i].innerText = ` = ${_result.toFixed(1)}cc/hr `;
     }

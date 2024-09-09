a04b05a1a4f7b4a2af52051a187a269eeb40f273
K0KK0R0 <shadowstarDeveloper@gmail.com>
2024-09-10 01:16:30 +0900

    20240910_01

--- Script.js
+++ Script.js
@@ -90,9 +90,15 @@ function addItem( object ){
          drugnameTxt.value = object.drugName;
          drugdoseTxt.value = object.drugDose;
          dosebt.value = 1;
+         if (object.drugGram == "mg") {
+             dosebt.value = 1;
+         }
          if (object.drugGram == "mcg") {
-             drugdoseTxt.value = object.drugDose;// * 0.001;
+             //drugdoseTxt.value = object.drugDose;// * 0.001;
              dosebt.value = 1000;
+         }
+         if (object.drugGram == "unit") {
+             dosebt.value = 1;
          }
          fluidccTxt.value = object.fluidTotalcc;
          drugspeedTxt.value = object.drugSpeed;
@@ -123,84 +129,6 @@ function addItem( object ){
  * @param {InputEvent} e 
  */
 
-
-
-/*function drugsearch(e) {
-    let parent = e.currentTarget.parentNode.parentNode.parentNode.parentNode;
-    _drugnametxt = e.currentTarget.value;
-
-    _drugdose = parent.querySelector(".drugdose");
-    _fluidcc = parent.querySelector(".fluidcc");
-
-    _drugspeed = parent.parentNode.querySelector(".drugspeed");
-    _drugspeedtxt = parent.querySelector(".drugspeedtxt");
-    
-
-    switch (_drugnametxt) {
-        *//*
-                            <option value="Norepinephrine"></option>
-                            <option value="Dobutamine"></option>
-                            <option value="Dopamix(premix)"></option>
-                            <option value="Remifentanil"></option>
-                            <option value="Propofol"></option>
-                            <option value="Midazolam"></option>
-                            <option value="Precedex"></option>
-        *//*
-        //innotropics
-        case "Norepinephrine":
-            _drugdose.value = 4;
-            _fluidcc.value = 50;
-            _drugspeed.value = 0.02;
-            _drugspeedtxt.value = "mcg/kg/min";
-            break;
-
-        case "Dobutamine":
-            _drugdose.value = 400;
-            _fluidcc.value = 50;
-            _drugspeed.value = 5;
-            _drugspeedtxt.value = "mcg/kg/min";
-            break;
-
-        case "Dopamix(premix)":
-            _drugdose.value = 400;
-            _fluidcc.value = 200;
-            _drugspeed.value = 5;
-            _drugspeedtxt.value = "mcg/kg/min";
-            break;
-
-        //sedation
-        case "Remifentanil":
-            _drugdose.value = 5;
-            _fluidcc.value = 50;
-            _drugspeed.value = 0.05
-            _drugspeedtxt.value = "mcg/kg/min";
-            break;
-        case "Propofol":
-            _drugdose.value = 120;
-            _fluidcc.value = 15;
-            _drugspeed.value = 0.3;
-            _drugspeedtxt.value = "mg/kg/hr";
-            break;
-        case "Midazolam":
-            _drugdose.value = 30;
-            _fluidcc.value = 30;
-            _drugspeed.value = 0.02;
-            _drugspeedtxt.value = "mg/kg/hr";
-            break;
-        case "Precedex":
-            _drugdose.value = 0.4;
-            _fluidcc.value = 100;
-            _drugspeed.value = 0.2;
-            _drugspeedtxt.value = "mcg/kg/hr";
-            break;
-
-        default:
-            break;
-
-    }
-    update();
-    //
-}*/
 
 const update = function (value) {
     //console.log(BWTinput.value);
@@ -263,17 +191,24 @@ const update = function (value) {
             case "unit/min":
                 _min = 60;
                 _dose = 1000;
+                _bwt = 1;
                 break;
             case "unit/hr":
                 _min = 60;
                 _dose = 1000;
+                _bwt = 1;
                 break;
         }
         //0.1mcg/kg/min = cc/hr
         // 0.1mcg/kg/min*hr = cc
         //0.0001  * drugdose / bwt / 60 //mcg는 *1000
         //(data.drugSpeed * data.fluidTotalcc * Bwt * 60) / (data.drugDose * dose);
-        let _result = (_drugspeed * _fluidcc * _bwt * _min) / (_drugdose * _dose) * 1000 / _doseBt;
+        let _result = (_drugspeed * _fluidcc * _bwt * _min) / (_drugdose * _dose) * _doseBt;
+        if (_doseBt < 0) {
+            //unit일 경우
+            _result = (_drugspeed * _fluidcc * _bwt * _min) / (_drugdose * _dose) * -_doseBt;
+        }
+        
 
         drugResult[i].innerText = ` = ${_result.toFixed(1)}cc/hr `;
     }
